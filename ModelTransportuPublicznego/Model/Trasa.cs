866f7a467fc2ae346dcfb05345857650520d585c
Maciej Gniadzik <gniadzikmaciej1@gmail.com>
2019-04-06 21:08:59 +0200

    Dodanie punktów trasy dla uatrakcyjnienia wyglądu map wynikowych.

--- ModelTransportuPublicznego/Model/Trasa.cs
+++ ModelTransportuPublicznego/Model/Trasa.cs
@@ -1,3 +1,6 @@
+using System.Collections.Generic;
+using System.Drawing;
+
 namespace ModelTransportuPublicznego.Model {
     public class Trasa {
         protected string nazwaTrasy;
@@ -5,6 +8,7 @@ namespace ModelTransportuPublicznego.Model {
         protected Przystanek.Przystanek przystanekPrawy;
         protected int sTrasy;
         protected double vMaxTrasy;
+        protected List<Point> punktyTrasy;
 
         public string NazwaTrasy => nazwaTrasy;
         public Przystanek.Przystanek PrzystanekLewy => przystanekLewy;
@@ -13,16 +17,36 @@ namespace ModelTransportuPublicznego.Model {
         public int DystansTrasy => sTrasy;
         public virtual Trasa TrasaOdwrotna => OdwrocTrase();
 
-        public Trasa(string nazwaTrasy, Przystanek.Przystanek przystanekLewy, Przystanek.Przystanek przystanekPrawy, int sTrasy, double vMaxTrasy) {
+        public Trasa()
+        {
+            punktyTrasy = new List<Point>();
+        }
+
+        public Trasa(string nazwaTrasy, Przystanek.Przystanek przystanekLewy, Przystanek.Przystanek przystanekPrawy, int sTrasy, double vMaxTrasy, IEnumerable<Point> punktyTrasy = null) {
             this.nazwaTrasy = nazwaTrasy;
             this.przystanekLewy = przystanekLewy;
             this.przystanekPrawy = przystanekPrawy;
             this.sTrasy = sTrasy;
             this.vMaxTrasy = vMaxTrasy;
+
+            if (punktyTrasy != null)
+            {
+                foreach (var p in punktyTrasy)
+                {
+                    this.punktyTrasy.Add(p);
+                }
+            }
         }
 
         protected virtual Trasa OdwrocTrase() {
-            return new Trasa(nazwaTrasy, PrzystanekPrawy, przystanekLewy, sTrasy, vMaxTrasy);
+            List<Point> punkty = new List<Point>();
+
+            foreach (var p in punktyTrasy)
+            {
+                punkty.Insert(0, p);
+            }
+
+            return new Trasa(nazwaTrasy, PrzystanekPrawy, przystanekLewy, sTrasy, vMaxTrasy, punkty);
         }
     }
 }
